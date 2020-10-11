7a9c88c66557f4491c0522725bc07563c0b932c5
Max Bay <cwang167@my.centennialcollege.ca>
2020-10-11 12:55:12 -0400

    2nd Update
    Chest added

---
+++ Assets/Scripts/LeftWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftWall : MonoBehaviour
+{
+    private Cinemachine.CinemachineCollisionImpulseSource MyInpulse;
+
+    private void Start()
+    {
+        MyInpulse = GetComponent<Cinemachine.CinemachineCollisionImpulseSource>();
+    }
+
+    //hit the ground, shake vCamera
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Tilemap")
+        {
+            MyInpulse.GenerateImpulse();
+        }
+    }
+
+}
