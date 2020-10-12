82f7f79bdd503572653b73f15489cbe8ae76c69c
Max Bay <cwang167@my.centennialcollege.ca>
2020-10-12 09:34:48 -0400

    6th Update
    shoot trigger script bug fixed

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -7,16 +7,37 @@ public class Cloud : MonoBehaviour
 {
     [SerializeField] private float fadeSpeed;
     private Material myMaterial;
+    private Color myColor;
+    private bool standOnCloud = false;
 
     private void Start()
     {
         myMaterial = GetComponent<SpriteRenderer>().material;
+        myColor = GetComponent<SpriteRenderer>().material.color;
     }
 
     //player stay, fade out
     private void OnCollisionStay2D(Collision2D collision)
     {
-        myMaterial.DOColor(Color.clear, fadeSpeed);
-        Destroy(this.gameObject, fadeSpeed - 0.5f);
+        if (collision.gameObject.tag == "Player")
+        {
+            standOnCloud = true;
+            
+            myMaterial.DOColor(Color.clear, fadeSpeed);
+            if (myColor.a<0.01f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    //player exit, fade in
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //standOnCloud = false;
+            myMaterial.DOColor(myColor, fadeSpeed);
+        }
     }
 }
--- Assets/Scripts/SpikeShootTrigger.cs
+++ Assets/Scripts/SpikeShootTrigger.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class SpikeShootTrigger : MonoBehaviour
 {
+    private bool sendOnce = true;
+
     //player enter the zone, shoot
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && sendOnce)
         {
             BroadcastMessage("WaitForShoot", true);
+            sendOnce = false;
         }
     }
 
